c53147d116c12ee5c6f2d590ff5076b36f19d2be
Ederson Torresini <boidacarapreta@gmail.com>
2022-05-02 21:08:19 -0300

    Testar repositórios das equipes como submódulos

--- servidor.js
+++
@@ -1,6 +0,0 @@
-const express = require("express");
-const app = express();
-const PORT = process.env.PORT || 3000;
-
-app.use(express.static("./cliente"));
-app.listen(PORT, () => console.log(`Server listening on port ${PORT}!`));
