611a4dd096099e43f852531ce4e5c4279ee97390
Ederson Torresini <boidacarapreta@gmail.com>
2022-05-23 10:18:12 +0000

    Mover arquivos do cliente para raiz

--- servidor.js
+++ servidor.js
@@ -3,5 +3,5 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static("./cliente"));
+app.use(express.static("./"));
 app.listen(PORT, () => console.log(`Server listening on port ${PORT}!`));
