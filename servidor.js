d6062ad063a4c690a7d2b1a595e5652c0b8da201
Ederson Torresini <boidacarapreta@gmail.com>
2022-06-21 14:49:09 -0300

    Remover servidor HTTP para Heroku

--- servidor.js
+++ servidor.js
@@ -54,5 +54,5 @@ io.on("connection", (socket) => {
 });
 
 // Abrir porta para HTTPS/WSS
-app.use(express.static("./"));
+// app.use(express.static("./"));
 server.listen(PORT, () => console.log(`Server listening on port ${PORT}!`));
