aca274964016b8a0f71e3b50af921498327994c8
Ederson Torresini <boidacarapreta@gmail.com>
2022-07-12 21:50:05 -0300

    Política cache-first-network-later #45

--- sw.js
+++ sw.js
@@ -1,59 +1,22 @@
-const version = "0.1.2";
+const version = "1.0.0";
 const cacheName = "cliente.ifsc.cloud";
-const precacheResources = [
-  "./assets/ARCas.png",
-  "./assets/baixo.png",
-  "./assets/cena0.png",
-  "./assets/cena1.json",
-  "./assets/cena1.mp3",
-  "./assets/cena2.png",
-  "./assets/cima.png",
-  "./assets/direita.png",
-  "./assets/fullscreen.png",
-  "./assets/grade.png",
-  "./assets/logo-128.png",
-  "./assets/logo-192.png",
-  "./assets/logo-256.png",
-  "./assets/logo-384.png",
-  "./assets/logo-512.png",
-  "./assets/parede.mp3",
-  "./assets/player1.png",
-  "./assets/player2.png",
-  "./assets/terreno.png",
-  "./assets/voz.mp3",
-  "./favicon.ico",
-  "./index.html",
-  "./js/cena0.js",
-  "./js/cena1.js",
-  "./js/cena2.js",
-  "./js/index.js",
-  "./js/phaser.min.js",
-  "./main.css",
-  "./manifest.json",
-  "./sw.js",
-];
 
 self.addEventListener("install", (event) => {
-  console.log("Service worker install event!");
-  event.waitUntil(
-    caches.open(cacheName).then((cache) => {
-      return cache.addAll(precacheResources).then(() => self.skipWaiting());
-    })
-  );
-});
-
-self.addEventListener("activate", (event) => {
-  event.waitUntil(self.clients.claim());
+  event.waitUntil(caches.open(cacheName));
 });
 
 self.addEventListener("fetch", (event) => {
-  console.log("Fetch intercepted for: ", event.request.url);
   event.respondWith(
-    caches
-      .open(cacheName)
-      .then((cache) => cache.match(event.request, { ignoreSearch: true }))
-      .then((response) => {
-        return response || fetch(event.request);
-      })
+    caches.open(cacheName).then((cache) => {
+      return cache.match(event.request.url).then((cachedResponse) => {
+        if (cachedResponse) {
+          return cachedResponse;
+        }
+        return fetch(event.request).then((fetchedResponse) => {
+          cache.put(event.request, fetchedResponse.clone());
+          return fetchedResponse;
+        });
+      });
+    })
   );
 });
