bb8675292a9604a36dd280dedb4fd91a5e35cd77
Ederson Torresini <boidacarapreta@gmail.com>
2022-07-15 11:18:20 +0000

    Destacar jogo escolihod no centro da tela

--- index.js
+++ index.js
@@ -24,26 +24,35 @@ const game = new Phaser.Game(config);
 function preload() {
   this.load.image("fortfight", "./assets/fortfight.png");
   this.load.image("gravity-falls-game", "./assets/gravity-falls-game.png");
-  this.load.image("killer-run", "./assets/killer-run.png");
-  this.load.image("princesas-perdidas", "./assets/princesas-perdidas.png");
-  this.load.image("soccer-simulator", "./assets/soccer-simulator.png");
+  this.load.image("gravity-falls-game-qrcode", "./assets/gravity-falls-game-qrcode.png");
 }
 
 function create() {
   var jogos = [
     {
+      indice: "fortfight",
+      //qrcode: "gravity-falls-game-qrcode",
+      titulo: "FortFight",
+      x: 400,
+      y: 200,
+      url: "https://fortfight.ifsc.cloud",
+    },
+    {
       indice: "gravity-falls-game",
+      qrcode: "gravity-falls-game-qrcode",
       titulo: "Gravity Falls Game",
       x: 200,
       y: 200,
-      url: "http://gravityfallsgame.ifsc.cloud",
+      url: "https://gravityfallsgame.ifsc.cloud",
     },
   ];
+  var escolha = undefined
 
   jogos.forEach((jogo) => {
     jogo.imagem = this.add
       .image(jogo.x, jogo.y, jogo.indice, 0)
       .setInteractive();
+
     jogo.texto = this.add
       .text(jogo.x - 128, jogo.y + 128, jogo.titulo, {
         fontFamily: "monospace",
@@ -54,18 +63,26 @@ function create() {
       .setScrollFactor(0);
 
     jogo.imagem.on("pointerdown", () => {
-      jogos.forEach((jogo) => {
-        jogo.imagem.setVisible(false);
-        jogo.texto.setVisible(false);
-      });
-    });
-    jogo.imagem.on("pointerout", () => {
-      jogos.forEach((jogo) => {
-        jogo.imagem.setVisible(true);
-        jogo.texto.setVisible(true);
-      });
+      if (escolha) {
+        jogos.forEach((jogo) => {
+          jogo.imagem.setVisible(true);
+          jogo.texto.setVisible(true);
+        });
+        escolha = undefined
+        jogo.imagem.x = jogo.x
+        jogo.imagem.y = jogo.y
+      } else {
+        jogos.forEach((jogo) => {
+          jogo.imagem.setVisible(false);
+          jogo.texto.setVisible(false);
+        });
+        escolha = jogo.indice
+        jogo.imagem.setVisible(true)
+        jogo.imagem.x = 240
+        jogo.imagem.y = 960
+      }
     });
   });
 }
 
-function update() {}
+function update() { }
