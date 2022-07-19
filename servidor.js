de4b2adfc26f24563c60b8ddc62a3a24781826b7
Ederson Torresini <boidacarapreta@gmail.com>
2022-07-18 22:12:27 -0300

    Remover arquivos desnecesários

--- servidor.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const app = express();
-const server = require("http").Server(app);
-const PORT = process.env.PORT || 3000;
-
-app.use(express.static("./"))
-server.listen(PORT, () => console.log(`Server listening on port ${PORT}!`));
